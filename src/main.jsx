a1673837ae31d60951aa8bd2a556113647ccdecc
unknown <corvel.jeff@gmail.com>
2026-05-01 18:53:32 -0300

    v3.6 - fix: corrigir extensao useAuth para .jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { AuthProvider } from './hooks/useAuth.js'
+import { AuthProvider } from './hooks/useAuth.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
